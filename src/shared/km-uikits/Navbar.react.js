0f1ed990d7b2944297f6ed9a2c36bdd28d523ece
ChinCluBi <chinnaporn.s@ku.th>
2017-04-30 22:13:04 +0700

    :lipstick: cart turn to red when menu is more than 0

--- src/shared/km-uikits/Navbar.react.js
+++ src/shared/km-uikits/Navbar.react.js
@@ -1,34 +1,39 @@
 import PropTypes from 'prop-types'
 import React from 'react'
+import className from 'classnames'
+import styles from './NavBar.styl'
 
-const Navbar = (props) => (
-  <nav className='nav has-shadow'>
-    <div className='container'>
-      <div className='nav-left'>
-        <a href='/' className='nav-item'>
-          <img src='/logo.png' />
-        </a>
+const Navbar = (props) => {
+  const menuLength = Object.keys(props.carts).length
+  return (
+    <nav className='nav has-shadow'>
+      <div className='container'>
+        <div className='nav-left'>
+          <a href='/' className='nav-item'>
+            <img src='/logo.png' />
+          </a>
+        </div>
+        <div className='nav-right nav-menu'>
+          <a className={className('nav-item', 'is-tab', { [styles['cart__not-empty']]: menuLength > 0 })}>
+            <span className='icon is-danger' style={{ marginRight: '8px' }}>
+              <i className='fa fa-shopping-cart' />
+            </span>
+            <span>{menuLength}</span>
+          </a>
+          <a className='nav-item is-tab'>
+            <span className='icon is-danger' style={{ marginRight: '8px' }}>
+              <i className='fa fa-user' />
+            </span>
+            <span>Profile</span>
+          </a>
+          <a className='nav-item is-tab'>
+            Logout
+          </a>
+        </div>
       </div>
-      <div className='nav-right nav-menu'>
-        <a className='nav-item is-tab'>
-          <span className='icon is-danger' style={{ marginRight: '8px' }}>
-            <i className='fa fa-shopping-cart' />
-          </span>
-          <span>{Object.keys(props.carts).length}</span>
-        </a>
-        <a className='nav-item is-tab'>
-          <span className='icon is-danger' style={{ marginRight: '8px' }}>
-            <i className='fa fa-user' />
-          </span>
-          <span>Profile</span>
-        </a>
-        <a className='nav-item is-tab'>
-          Logout
-        </a>
-      </div>
-    </div>
-  </nav>
-)
+    </nav>
+  )
+          }
 
 Navbar.propTypes = {
   carts: PropTypes.object.isRequired
